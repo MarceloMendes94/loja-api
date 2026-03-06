ad7fdcfe6f44953ee08af95369093de8cb79fc00
marceloGlobalsys_notebook <marcelocelularxiaomi@gmail.com>
2026-03-06 17:17:10 -0300

    inicio da API /usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { usuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [usuarioModule],
 })
 export class AppModule {}
---
+++ src/usuario/usuario.repository.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class UsuarioRepository{
+    private usuarios:any[] = [];// esta errado pois  não devemos usar any em TS
+    async salvar(usuario){
+        this.usuarios.push(usuario);
+        console.log(this.usuarios);
+    }
+    async listar(){
+        return this.usuarios;
+    }
+}
